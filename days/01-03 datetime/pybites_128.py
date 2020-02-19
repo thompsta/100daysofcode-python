dae0bf701f576cd2291051c0fbdad970bcbf870a
Stan Thompson <61202843+thompsta@users.noreply.github.com>
2020-02-19 08:09:11 -0800

    Add files via upload

---
+++ days/01-03 datetime/pybites_128.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+THIS_YEAR = 2018
+
+date_val = '31 Oct, 1958'
+
+date_to_convert = '18/02/2014'
+
+def years_ago(date):
+    """Receives a date string of 'DD MMM, YYYY', for example: 8 Aug, 2015
+       Convert this date str to a datetime object (use strptime).
+       Then extract the year from the obtained datetime object and subtract
+       it from the THIS_YEAR constant above, returning the int difference.
+       So in this example you would get: 2018 - 2015 = 3"""
+    new_date = datetime.strptime(date, '%d %b, %Y')
+    return THIS_YEAR - new_date.year
+
+
+def convert_eu_to_us_date(date):
+    """Receives a date string in European format of dd/mm/yyyy, e.g. 11/03/2002
+       Convert it to an American date: mm/dd/yyyy (in this case 03/11/2002).
+       To enforce the use of datetime's strptime / strftime (over slicing)
+       the tests check if a ValueError is raised for invalid day/month/year
+       ranges (no need to code this, datetime does this out of the box)"""
+    convert_date = datetime.strptime(date, '%d/%m/%Y')
+    final = convert_date.strftime('%m/%d/%Y')
+    return final
+
+print(years_ago(date_val))
+
+print(convert_eu_to_us_date(date_to_convert))
---
+++ days/01-03 datetime/pybites_67.py
@@ -0,0 +1,22 @@
+from datetime import date, timedelta
+
+start_100days = date(2017, 3, 30)
+pybites_founded = date(2016, 12, 19)
+pycon_date = date(2018, 5, 8)
+
+
+def get_hundred_days_end_date():
+    """Return a string of yyyy-mm-dd"""
+    tdelta = timedelta(days=100)
+    return str(start_100days + tdelta)
+    
+
+
+def get_days_between_pb_start_first_joint_pycon():
+    """Return the int number of days"""
+    datediff = pycon_date - pybites_founded
+    return datediff.days
+
+print(get_hundred_days_end_date())
+
+print(get_days_between_pb_start_first_joint_pycon())
