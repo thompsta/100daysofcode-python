7f6da8e5443c71033a42449509a459f2da55ec65
Stan Thompson <61202843+thompsta@users.noreply.github.com>
2020-02-28 08:54:51 -0800

    Create DnD.py

---
+++ days/13-15 textgames/DnD.py
@@ -0,0 +1,62 @@
+import random
+from actors import Creature, Dragon, Wizard
+
+
+def main():
+    print_header()
+    game_loop()
+
+def print_header():
+    print('----------------------------')
+    print('        WIZARD GAME')
+    print('----------------------------')
+    print()
+
+def game_loop():
+    creatures = [
+        Creature('Bat', 5),
+        Creature('Toad', 1),
+        Creature('Tiger', 12),
+        Dragon('Black Dragon', 50, scaliness=2, breaths_fire=False),
+        Wizard('Evil wizard', 1000)
+    ]
+
+    hero = Wizard('Gandolf', 75)
+
+    while True:
+
+        # Randomly choose a creature
+        # random.choice will randomly choose a value from a collection object
+        active_creature = random.choice(creatures)
+
+        print('A {} of level {} has appeared from a dark and foggy forest...'
+        .format(active_creature.name, active_creature.level))
+        print()
+
+        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
+        if cmd == 'a':
+            if hero.attack(active_creature):
+                print("The wizard defeated {}!".format(active_creature.name)) 
+            else:
+                print("The wizard has been defeated by the powerful {}!!".format(active_creature.name))
+        elif cmd == 'r':
+            print('The wizard has become unsure of his power and flees!!!')
+        elif cmd == 'l':
+            print('The wizard {} takes in the surroundings and sees:'
+                    .format(hero.name))
+            for c in creatures:
+                print(" * {} level of {}".format(
+                    c.name, c.level
+                ))
+        else:
+            print("Ok, exiting game... bye!")
+            break
+
+        if not creatures:
+            print("You've defeated all the creatures, well done!")
+            break
+            
+        print()
+
+if __name__ == '__main__':
+    main()
